941f44745b74c198e339de92d4300549cc1925e6
Link4Electronics <link4electronics@gmail.com>
2026-03-29 13:03:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,21 +3,21 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q play-emu-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/org.purei.Play.png
 export DESKTOP=/usr/share/applications/org.purei.Play.desktop
 export STARTUPWMCLASS=Play
-export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/Play
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/Play /usr/lib/libopenal.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
